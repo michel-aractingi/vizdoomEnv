53219a2c4c92717f4c46886a974795eb66f1ee2d
michel.aractingi <michel.aractingi@naverlabs.com>
2020-03-12 17:37:46 +0100

    clean test scripts

--- test_scripts/test_vizdoom.py
+++ test_scripts/test_vizdoom.py
@@ -6,48 +6,10 @@ import time
 import argparse
 import os 
 import cv2
-from utils import detect_collision
+from utils import *
 import numpy as np
 
-joystick_actions = [vizdoom.Button.MOVE_FORWARD, vizdoom.Button.MOVE_BACKWARD,
-                    vizdoom.Button.TURN_LEFT, vizdoom.Button.TURN_RIGHT, vizdoom.Button.ATTACK]
-
-ammos = [GameVariable.AMMO0, GameVariable.AMMO1, GameVariable.AMMO2, GameVariable.AMMO3, GameVariable.AMMO4, GameVariable.AMMO5, GameVariable.AMMO6, GameVariable.AMMO7, GameVariable.AMMO8, GameVariable.AMMO9] 
-
-def str2bool(x):  return x.lower() in ('true','1','t','yes','y')
-
-def render_buffers(state):
-
-    depth = state.depth_buffer
-    if depth is not None:
-        cv2.imshow('ViZDoom Depth Buffer', depth)
-
-    # Labels buffer, always in 8-bit gray channel format.
-    # Shows only visible game objects (enemies, pickups, exploding barrels etc.), each with unique label.
-    # Labels data are available in state.labels, also see labels.py example.
-    labels = state.labels_buffer
-    if labels is not None:
-        cv2.imshow('ViZDoom Labels Buffer', labels)
-
-    # Map buffer, in the same format as screen buffer.
-    # Shows top down map of the current episode/level.
-    automap = state.automap_buffer
-    if automap is not None:
-        cv2.imshow('ViZDoom Map Buffer', np.rollaxis(automap,0,3))
-
-    cv2.waitKey(28)
-
-def get_distance_to_goals(game):
-
-    i = list(range(5))
-    state = game.get_state()
-    actor_pos = np.array([game.get_game_variable(GameVariable.POSITION_X), game.get_game_variable(GameVariable.POSITION_Y)])
-    goal_pos = np.array([[doom_fixed_to_float(state.game_variables[2 * it + 2]), \
-                          doom_fixed_to_float(state.game_variables[2 * it + 3])] for it in i])
-    N = doom_fixed_to_float(state.game_variables[-1])
-    l2_dists = [1 - np.linalg.norm(actor_pos - goal_pos[it]) / N for it in i]
-
-    return l2_dists
+CONFIG_DIR = '/home/maractin/Workspace/vizdoomEnv/scenarios/'
 
 def random_env(game):
 
@@ -74,44 +36,24 @@ def random_env(game):
 
 def keyboard_env(game):
 
-    game.set_window_visible(True)
     game.set_mode(vizdoom.Mode.SPECTATOR)
     game.set_screen_resolution(vizdoom.ScreenResolution.RES_1280X800)
-
     game.init()
+
     episodes = 10
-
+   
     for i in range(episodes):
-        print("Episode #" + str(i + 1))
 
+        print("Episode #" + str(i + 1))
         game.new_episode()
-        old_pos = np.array([game.get_game_variable(GameVariable.POSITION_X),
-                            game.get_game_variable(GameVariable.POSITION_Y)])
         while not game.is_episode_finished():
             state = game.get_state()
-            render_buffers(state)
             game.advance_action()
             last_action = game.get_last_action()
             reward = game.get_last_reward()
-            if detect_collision(game):
-                print("Collision Occuring...........!!!!!!")
-            print("State #" + str(state.number))
-            if not game.is_episode_finished():
-                print('goal_cond reward: ', get_distance_to_goals(game))
-                actor_pos = np.array([game.get_game_variable(GameVariable.POSITION_X),
-                                      game.get_game_variable(GameVariable.POSITION_Y)])
-                print("Actor Pos: ", actor_pos)
-                print('Actor dist:', np.linalg.norm(actor_pos - old_pos))
-                old_pos = np.array([game.get_game_variable(GameVariable.POSITION_X),
-                                    game.get_game_variable(GameVariable.POSITION_Y)])
-                Ammo = [game.get_game_variable(a) for a in ammos]
-                print("Game variables ammo: ", Ammo)
-            print("Action:", last_action)
-            print("Reward:", reward)
-            print("Done Flag:", game.is_episode_finished())
+            print("State #{}, Action {}, Reward {}, Done {}".format(
+                str(state.number),last_action,reward,game.is_episode_finished()))
             print("=====================")
-            if state.number == 500000:
-                import pudb; pudb.set_trace()
         print("Episode finished!")
         print("Total reward:", game.get_total_reward())
         print("************************")
@@ -149,10 +91,10 @@ def joystick_env(game):
 def main(args):
 
     game = DoomGame()
-    game.load_config(os.path.join(args.configdir, args.config))
-    game.set_doom_scenario_path(os.path.join(args.configdir, args.wad))
+    game.set_window_visible(True)
+    game.load_config(os.path.join(CONFIG_DIR, args.config))
+    game.set_doom_scenario_path(os.path.join(CONFIG_DIR, args.wad))
     game.set_episode_timeout(4200)
-    game.set_automap_buffer_enabled(True)
     #game.init() don't initialize the game here in case each mode needs its own configuration
 
     if args.pudb:
@@ -174,15 +116,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser([])
     parser.add_argument('-pudb', type=str2bool, default=False,
                         help='Whether to use pudb or not')
-    parser.add_argument('--configdir', type=str, default="/home/maractin/Workspace/vizdoomEnv/scenarios/",
-                        help='Specify config file path')
-    parser.add_argument('--config', '-cfg', type=str, default="basic.cfg",
+    parser.add_argument('--config', type=str, default="basic.cfg",
                         help='Specify config file')
-    parser.add_argument('--wad', '-w', type=str, default="basic.wad",
+    parser.add_argument('--wad', type=str, default="basic.wad",
                         help='Specify Wad file')
     parser.add_argument('--mode', type=str, default='random',
                         help='Specify whether to run interactively through keyboard or joystick or with random actions')
 
     args = parser.parse_args()
-
+    
     main(args)
--- test_scripts/utils.py
+++ test_scripts/utils.py
@@ -1,4 +1,12 @@
 import numpy as np
+from vizdoom import * 
+
+joystick_actions = [vizdoom.Button.MOVE_FORWARD, vizdoom.Button.MOVE_BACKWARD,
+                    vizdoom.Button.TURN_LEFT, vizdoom.Button.TURN_RIGHT, vizdoom.Button.ATTACK]
+
+ammos = [GameVariable.AMMO0, GameVariable.AMMO1, GameVariable.AMMO2, GameVariable.AMMO3, GameVariable.AMMO4, GameVariable.AMMO5, GameVariable.AMMO6, GameVariable.AMMO7, GameVariable.AMMO8, GameVariable.AMMO9]
+
+def str2bool(x):  return x.lower() in ('true','1','t','yes','y')
 
 def compress_depthmap(d):
     return np.max(d,axis=0)
@@ -23,4 +31,50 @@ def detect_collision(game):
     if np.min(compressed_depth[crop_ratio:len_compressed - crop_ratio]) == 0 and is_last_action_forward(game):
         return True
     return False
+
+def plot_sectors(state):
+    import matplotlib.pyplot as plt
+    for s in state.sectors:
+        print("Sector floor height:", s.floor_height, "ceiling height:", s.ceiling_height)
+        print("Sector lines:", [(l.x1, l.y1, l.x2, l.y2, l.is_blocking) for l in s.lines])
+
+        # Plot sector on map
+        for l in s.lines:
+            if l.is_blocking:
+                plt.plot([l.x1, l.x2], [l.y1, l.y2], color='black', linewidth=2)
+
+    # Show map
+    plt.show()
+
+def render_buffers(state):
+
+    depth = state.depth_buffer
+    if depth is not None:
+        cv2.imshow('ViZDoom Depth Buffer', depth)
+
+    # Labels buffer, always in 8-bit gray channel format.
+    # Shows only visible game objects (enemies, pickups, exploding barrels etc.), each with unique label.
+    # Labels data are available in state.labels, also see labels.py example.
+    
+    labels = state.labels_buffer
+    if labels is not None:
+        cv2.imshow('ViZDoom Labels Buffer', labels)
+
+    # Map buffer, in the same format as screen buffer.
+    # Shows top down map of the current episode/level.
+    automap = state.automap_buffer
+    if automap is not None:
+        cv2.imshow('ViZDoom Map Buffer', np.rollaxis(automap,0,3))
+
+    cv2.waitKey(28)
+
+
+def color_labels(labels):
+    """
+    Walls are blue, floor/ceiling are red (OpenCV uses BGR).
+    """
+    tmp = np.stack([labels] * 3, -1)
+    tmp[labels == 0] = [255, 0, 0]
+    tmp[labels == 1] = [0, 0, 255]
+    return tmp
 
